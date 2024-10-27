9d184dd7d5e2ce468391c7765b1673263f751c98
Arshad-Shamim <arshadshamim555@gmail.com>
2024-10-27 19:26:26 +0530

    update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Signin from './pages/Signin.jsx'
+import Signin from './pages/signin.jsx'
 import {Routes,Route,Navigate} from 'react-router-dom';
 import Signup from './pages/Signup.jsx'
 
