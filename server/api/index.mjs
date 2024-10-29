c9868a5deabdae39eddee4a800147a4637efd0cc
Arshad-Shamim <arshadshamim555@gmail.com>
2024-10-29 19:31:07 +0530

    testing of email

--- server/api/index.mjs
+++ server/api/index.mjs
@@ -5,17 +5,6 @@ import { router as handleEmail }  from '../router/email.mjs';
 const app = express();
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("hello /");
-    req.end();
-})
-
-app.get("/demo",(req,res)=>{
-    res.send("hello /demo ");
-    req.end();
-})
-
-
 app.use("/email",handleEmail);
 
 app.listen(2000,(err)=>{
--- server/controller/email.mjs
+++ server/controller/email.mjs
@@ -1,6 +1,35 @@
+import nodemailer from 'nodemailer';
+
+function sendMail(receiver,subject,body){
+    const transpoter = nodemailer.createTransport({
+        service:"gmail",
+        auth:{
+            user:"arshadshmim786@gmail.com",
+            pass:"csmc zgbc kuby igrj"
+        }
+    });
+
+    const mailOptions = {
+        from:"arshadshmim786@gmail.com",
+        to:receiver,
+        subject:subject,
+        body:body
+    };
+
+    transpoter.sendMail(mailOptions,(err,info)=>{
+        if(err)
+            console.log(err);
+        else
+            console.log(info.response);
+    });
+}
+
 function sendEmail(req,res){
     const email = req.body.email;
-    console.log(email);
+    const subject = "testing....";
+    const body="good evening arshad";
+    const recevier = "arshadshamim555@gmail.com";
+    sendMail(recevier,subject,body);
     req.end();
 }
 
