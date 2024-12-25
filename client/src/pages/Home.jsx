2a1dc47af58fba9f38c4e0aaa821188147de2ace
Arshad-Shamim <arshadshamim555@gmail.com>
2024-12-26 00:05:54 +0530

    resolve rcrolling effect

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -323,42 +323,43 @@ function notifyFailer(data){
                 </select>
               </div>
             </div>
-
-            <table class="table table-bordered table-hover table-responsive ">
-              <caption>User Account Database</caption>
-              <thead className='table-primary'>
-                <tr>
-                  <th scope="col">S. No.</th>
-                  <th scope="col">URL</th>
-                  <th scope="col">USERNAME IDENTIFIER</th>
-                  <th scope="col">PASSWORD</th>
-                  <th>ACTION</th>
-                </tr>
-              </thead>
-              <tbody>
-                  {
-                    data.map((obj,index)=>{
-                      return (
-                        <tr>
-                          <th scope={`${index}`}>{index+1}</th>
-                          <td><a href={obj.url}  className='text-dark'>{obj.url}</a></td>
-                          <td>{obj.user_indentifier}</td>
-                          <td>
-                            {tablepws==index?table_pws_fn(obj.pws):<span onClick={()=>setTablepws(index)} className='text-primary btn m-0 p-0 text-start ms-2'>view</span>}
-                          </td>
-                          <td className='ms-2 btn m-0 p-0'>
-                            <div onClick={()=>deleteTablerow(obj.url)}>
-                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-                                <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
-                              </svg>
-                            </div>
-                          </td>
-                        </tr>
-                      )
-                    })
-                  }
-              </tbody>
-            </table>
+            <div className='container' style={{"overflowY":"scroll"}}>
+              <table class="table table-bordered table-hover">
+                <caption>User Account Database</caption>
+                <thead className='table-primary'>
+                  <tr>
+                    <th scope="col">S. No.</th>
+                    <th scope="col">URL</th>
+                    <th scope="col">USERNAME IDENTIFIER</th>
+                    <th scope="col">PASSWORD</th>
+                    <th>ACTION</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    {
+                      data.map((obj,index)=>{
+                        return (
+                          <tr>
+                            <th scope={`${index}`}>{index+1}</th>
+                            <td><a href={obj.url}  className='text-dark'>{obj.url}</a></td>
+                            <td>{obj.user_indentifier}</td>
+                            <td>
+                              {tablepws==index?table_pws_fn(obj.pws):<span onClick={()=>setTablepws(index)} className='text-primary btn m-0 p-0 text-start ms-2'>view</span>}
+                            </td>
+                            <td className='ms-2 btn m-0 p-0'>
+                              <div onClick={()=>deleteTablerow(obj.url)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                  <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+                                </svg>
+                              </div>
+                            </td>
+                          </tr>
+                        )
+                      })
+                    }
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
 
